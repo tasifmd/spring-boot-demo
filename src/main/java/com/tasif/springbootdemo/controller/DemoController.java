1e5a88204dc2b7e2432c2226f265b68ccb21436e
tasifmd <tasifmd96@gmail.com>
2020-09-30 15:18:23 +0530

    demo

---
+++ src/main/java/com/tasif/springbootdemo/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.tasif.springbootdemo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+	@GetMapping
+	public ResponseEntity<String> printTest() {
+		return new ResponseEntity<>("Hi demo controller", HttpStatus.OK);
+
+	}
+}
